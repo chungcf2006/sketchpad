b696cea6b72ba7013599b00ed84f1a33b1f67716
John Chung <chungcf2006@gmail.com>
2018-05-02 01:01:40 +0800

    Home Page

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@ html body {
   padding: 0;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', 'Myriad Pro', 'Lucida Sans', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="hello">
+  <div class="dialog">
     <h1>Welcome to Online Sketchpad</h1>
+    <div class="row">
+      <b-button variant="success" @click="join()">Join</b-button>
+      <b-button variant="primary" @click="create()">Create</b-button>
+    </div>
   </div>
 </template>
 
@@ -9,14 +13,48 @@ export default {
   name: 'Welcome',
   props: {
     msg: String
+  },
+  methods: {
+    join () {
+      console.log('hi')
+    },
+    create () {
+      console.log('hi2')
+    },
   }
 }
 </script>
 
-<style scoped>
-  h1 {
-    margin: 0;
+<style lang="scss" scoped>
+
+
+  .dialog {
+    position: absolute;
+    margin: auto;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    max-height: 25%;
+    width: 75%;
     padding-top: 0.5em;
     padding-bottom: 0.5em;
+    .row {
+      display: flex;
+      @keyframes fadein {
+        0%   {top: 1em; opacity: 0;}
+        100% {top: 0; opacity: 100%;}
+      }
+      >* {
+        position: relative;
+        width: 100%;
+        flex-basis: calc(50% - 0.5em);
+        font-size: 3em;
+        margin: 0.25em;
+        padding: 1em;
+        animation-name: fadein;
+        animation-duration: 1s;
+      }
+    }
   }
 </style>
